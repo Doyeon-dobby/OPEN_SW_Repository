9f9fa2c591553f2dd61fe6a5bb0e038ea41763f5
Doyeon-dobby <eowjs2461@naver.com>
2023-06-18 02:43:31 +0900

    create Score files

---
+++ Score.h
@@ -0,0 +1,22 @@
+//#pragma once
+#ifndef SCORE_H
+#define SCORE_H
+
+#define MOSTERKILL 10000
+#define BASICBLOCK 50
+
+#define COPPER 100
+#define SILVER 200
+#define GOLD 500
+#define CRYSTAL 1000
+#define DIAMOND 2000
+#define S_CRAETUM 3000
+#define S_SELANUM 4000
+#define S_CRIPTON 5000
+int GetSocre(int info);
+void ShowSocre();
+int ReturnScore(int action);
+
+
+#endif // !SCORE_H
+
